4777e3ac747ad0a213021f0e3f04b70973e30605
Sebastian Fiault (gamagos) <gamagos53@gmail.com>
2026-07-18 15:49:54 +0200

    Fixed typos

--- Dev/Build Scripts/build_linux_debug.sh
+++ Dev/Build Scripts/build_linux_debug.sh
@@ -2,8 +2,11 @@
 
 # (C) Sebastian Fiault
 
-cmake -S "../../" --preset Linux-Debug --fresh
+echo "[WARNING] This Script needs to be run from GalaxyCollision/Dev/Build Scripts/ to funtion properly!\n Running from different locations will result in improper behaviour" #TODO find way to make script automatically cd to it's location
 
+cd ../..
+cmake --preset Linux-Debug --fresh
 cmake --build --preset Linux-Debug
+cd "Dev/Build Scripts"
 
 #TODO document this so that people can build it on GitHub
